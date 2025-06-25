6f9fdf724b15ccaa18eb108d84d59f8580350358
Sandro sulava <sandrosulava2@gmail.com>
2025-06-25 19:35:02 +0400

    feature
    added js code
    changed color
    added index ids

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const fund = document.querySelector("#fund");
+const remove = document.querySelector("#removeBtn");
+
+removeBtn.addEventListener("click", function () {
+  fund.classList.add("hidden");
+});
