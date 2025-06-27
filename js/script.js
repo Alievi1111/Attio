d5c7e2147200f2e2f099ce47a54e05adaf9d7af6
Sandro sulava <sandrosulava2@gmail.com>
2025-06-27 10:49:00 +0400

    feature/responsive-header
    added js code
    added new class
    added new id

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const fund = document.querySelector("#fund");
 const removeBtn = document.querySelector("#removeBtn");
+const attio = document.querySelector("attio");
 
 removeBtn.addEventListener("click", function () {
   fund.classList.add("hidden");
+  attio.classList.add("attio-margin-top");
 });
